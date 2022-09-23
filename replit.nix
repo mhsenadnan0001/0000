83af483152d5d92a7529b99d633d87c795635522
mhsenadnan0001 <114256039+mhsenadnan0001@users.noreply.github.com>
2022-09-23 21:50:53 +0300

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+	deps = [
+		pkgs.psmisc
+pkgs.sudo
+pkgs.python39Full
+pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
+	];
+}
